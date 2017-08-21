cf1b5b4ab2d439865ac7b597e99abdd2fa232fe2
Sikander Chowhan <sikanderchowhan@gmail.com>
2017-08-21 23:58:13 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash = {}
+  data.each do |attribute, att_deet_hash|
+    att_deet_hash.each do |att_deet, pigeon_array|
+      pigeon_array.each do |pigeon|
+        if new_hash[pigeon].nil?
+          new_hash[pigeon] = {}
+        end
+        new_hash[pigeon][attribute] ||= []
+        new_hash[pigeon][attribute] << att_deet.to_s
+
+      end
+    end
+  end
+  new_hash
 end
